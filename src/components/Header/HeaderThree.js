be83f11ca869c0fdf9cba53147cad8ff712a8965
gghadiya2107 <gghadiya@deloitte.com>
2024-05-22 14:50:21 +0530

    downgrade Next varsion, sso login fix

--- src/components/Header/HeaderThree.js
+++ src/components/Header/HeaderThree.js
@@ -5,6 +5,9 @@ import { MdArrowDropDown } from "react-icons/md";
 import { useDispatch, useSelector } from 'react-redux';
 import { getCookieValues, removeCookie } from '@/utils/cookies';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
+
+
 const HeaderThree = () => {
   // const userData = useSelector((state) => state.authDetails?.data);
   const router = useRouter()
@@ -34,7 +37,7 @@ const HeaderThree = () => {
   }
   return (
     test &&  <Box sx={{ flexGrow: 1 }}>
-      <script src="https://himstaging2.hp.gov.in/nodeapi/iframe/sso-iframe.js" defer=""></script>
+      <Script src="https://himstaging2.hp.gov.in/nodeapi/iframe/sso-iframe.js" defer=""></Script>
       <AppBar className={style.threeAppBar} >
         <Toolbar style={{minHeight : "50px"}} className={style.mediaToolbar}>
             <div className={style.threeOuter}>
@@ -48,16 +51,7 @@ const HeaderThree = () => {
             {/* Himachal Pradesh Panchayati Raj */}
           </Typography>
 
-          {/* <Typography component="div" className={style.most}>
-            Most Searched
-          </Typography>
-          <Typography component="div" className={style.threeRight} >
-          Birth Certificate
-          </Typography>
-          <Typography component="div" className={style.threeRight2} >
-          Driving Licence
-          </Typography> */}
-          {/* {userData == null ? */}
+         
            <> <Typography component="div" className={style.threeRightLogin} style={{borderLeft : "none"}} onClick={() => getIframeSSO('10000080','login','Citizen')} >
           Citizen Login
           </Typography>
