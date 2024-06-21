87a75e6d63475e1f6ae688a55cb7b9acee236a71
gghadiya2107 <gghadiya@deloitte.com>
2024-06-21 13:43:45 +0530

    remove parichay

--- src/components/Header/HeaderThree.js
+++ src/components/Header/HeaderThree.js
@@ -54,7 +54,10 @@ const HeaderThree = () => {
            <> <Typography component="div" className={style.threeRightLogin} style={{borderLeft : "none"}} onClick={() => getIframeSSO('10000080','login','Citizen')} >
           Citizen Login
           </Typography>
-          <Typography component="div" className={style.threeRightLogin} onClick={() => window.open("https://sso.hp.gov.in/official/site/login?onboardingapp=urbanregister", "_blank", "noopener,noreferrer")} >
+          <Typography component="div" className={style.threeRightLogin} 
+          // onClick={() => window.open("https://sso.hp.gov.in/official/site/login?onboardingapp=urbanregister", "_blank", "noopener,noreferrer")} 
+          onClick={() => router.push("/registration")}
+          >
           Officer Login
           </Typography></> 
            {/* : 
