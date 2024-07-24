5b7b611b59ff90ed6b0c489bf502ac9c5d8a6da9
gghadiya2107 <gghadiya@deloitte.com>
2024-07-24 15:19:14 +0530

    Fix issue

--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import style from './datePicker.module.css'
 import { useRouter } from 'next/router';
 
 const DatePicker = ({title,subTitle,type,requried,placeholder, ...rest}) => {
   const router = useRouter();
   const { pathname } = router;
+  const dateInputRef = useRef(null);
+
+  const openDatePicker = () => {
+    if (dateInputRef.current) {
+      dateInputRef.current.click();
+    }
+  };
   return (
     <div style={{width :pathname?.includes("/edit-member") ? "80%" : "auto"}}>
     <p className={style.title}>{title}<span className={style.subtitle}> {subTitle}</span>{requried && <span className="requried"> *</span>}</p>
     <div className={style.inputField}>
-      <input className={style.input} type='date' max={new Date()?.toISOString().split('T')[0]}  placeholder={placeholder} {...rest} />
-      <span className={style.calendarIcon}>📅</span>
+  <input
+    className={style.input}
+    id="datepicker"
+    type='date'
+    max={new Date()?.toISOString().split('T')[0]}
+    placeholder={placeholder}
+    ref={dateInputRef}
+    style={{backgroundColor : "transparent"}}
+    {...rest}
+  />
+  <span
+    className={style.calendarIcon}
+    onClick={() => {
+      openDatePicker()
+    }}
+  >
+    📅
+  </span>
+</div>
 
-    </div>
   </div>
   )
 }
